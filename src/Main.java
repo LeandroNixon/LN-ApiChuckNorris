620d3080c0247824b035cfd53484302964f8c483
LEANDRO NIXON RIBEIRO DOS SANTOS HERMELIJN <leandro_hermelijn@estudante.sesisenai.org.br>
2024-02-26 19:42:29 -0300

    Primeira piada

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+
+
+            //Url da API do Chuck Norris
+            String apiURL = "https://api.chucknorris.io/jokes/random";
+            //fazendo a Requisição GET para a API
+            HttpURLConnection conexao = (HttpURLConnection) new URL(apiURL).openConnection();
+            conexao.setRequestMethod("GET");
+
+            //Lendo a resposta
+            BufferedReader leitor = new BufferedReader(new InputStreamReader(conexao.getInputStream()));
+            StringBuilder resposta = new StringBuilder();
+            String linha;
+            while ((linha = leitor.readLine()) != null){
+                resposta.append(linha);
+
+            }
+
+            leitor.close();
+            String piada = obterPiadaChuckNorris(resposta.toString());
+            System.out.println("Piada"+ piada);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private static String obterPiadaChuckNorris(String resposta)  {
+
+
+        int inicioDoIndice = resposta.indexOf("\"value\":") + ("\"value\":".length());
+        int fimDoIndice = resposta.lastIndexOf("\"");
+        return resposta.substring(inicioDoIndice,fimDoIndice);
+    }
+}
